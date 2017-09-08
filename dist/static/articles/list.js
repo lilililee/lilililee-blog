3606c88fe52d9dbd64b1d76ec5e93fbd07c8afaf
liguang <lilililee_hello@icloud.com>
2017-09-08 20:03:57 +0800

    again2

---
+++ dist/static/articles/list.js
@@ -0,0 +1,52 @@
+let list = {
+	introduce: {
+		id: '10001',
+		title: '个人简介',
+		time: '2017-9-2'
+	},
+	life: {
+		id: '20001',
+		title: '生活杂记',
+		time: '2017-9-2'
+	},
+
+	learing: {
+		'html': [
+			
+		],
+		'css': [
+			{
+				id: 'css-1',
+				title: '居中方案',
+				time: '2017-9-2'
+			}
+		],
+		'javascript': [
+			{
+				id: 'js-1',
+				title: '原型链讲解',
+				time: '2017-9-2'
+			},
+			{
+				id: 'js-2',
+				title: '字符串方法介绍',
+				time: '2017-9-2'
+			},
+			{
+				id: 'js-3',
+				title: '数组方法介绍',
+				time: '2017-9-2'
+			},
+			{
+				id: 'js-4',
+				title: 'DOM节点',
+				time: '2017-9-2'
+			}
+
+		],
+		'vue': []
+	}
+}
+
+
+export default list
